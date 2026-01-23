af441b2e2bcd336c0ebcf084f394310e7b379beb
Maksym <maksym.045z@gmail.com>
2026-01-23 10:29:31 +0100

    CI workflow  automated tests on pull request to develop branch

--- jest.config.js
+++ jest.config.js
@@ -5,21 +5,29 @@ const createJestConfig = nextJest({
 })
 
 const customJestConfig = {
-  testMatch: ['**/__tests__/**/*.{js,jsx,ts,tsx}'],
+  testMatch: ['**/__tests__/**/*.test.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/*.stories.tsx',
-    '!src/**/__tests__/**',
+    'app/**/*.{js,jsx,ts,tsx}',
+    '!app/**/*.d.ts',
+    '!app/**/*.stories.tsx',
+    '!app/**/__tests__/**',
   ],
   testPathIgnorePatterns: [
     './tests/'
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  }
 }
 
 module.exports = createJestConfig(customJestConfig)
